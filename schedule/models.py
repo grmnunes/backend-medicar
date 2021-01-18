051b58c44d89f4f9c8b661b33010d0addc638b7b
Germano Nunes <germanonunes@pop-os.localdomain>
2021-01-18 15:36:39 -0300

    migrations

--- schedule/models.py
+++ schedule/models.py
@@ -9,7 +9,7 @@ from .validators import validate_schedule_day
 class Schedule(models.Model):
     doctor = models.ForeignKey(Doctor, verbose_name='Médico', related_name='doctor', on_delete=models.CASCADE)
     day = models.DateField('Dia', validators=[validate_schedule_day])
-    is_completed = models.BooleanField(verbose_name='Agenda completa', default=False)
+    is_completed = models.BooleanField(verbose_name='Agenda completa', default=False, editable=False)
     
     class Meta:
         verbose_name = 'Agenda'
@@ -24,7 +24,7 @@ class Schedule(models.Model):
 class ScheduleHour(models.Model):
     schedule = models.ForeignKey(Schedule, verbose_name='Agenda',related_name='schedule', limit_choices_to = {'day__gte': date.today()}, on_delete=models.CASCADE)
     hour = models.TimeField(verbose_name='Hora', help_text='O horário deve conter apenas horas e minutos ex.: 12:45')
-    is_available = models.BooleanField(verbose_name='Disponível', default=True)
+    is_available = models.BooleanField(verbose_name='Disponível', default=True, editable=False)
     
     class Meta:
         verbose_name = 'Horário'
